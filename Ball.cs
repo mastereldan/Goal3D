67999121e3733559defbd29b115ecfd694bec6a2
Eldan Kahrović <40560732+mastereldan@users.noreply.github.com>
2019-01-01 19:23:56 +0100

    Add files via upload

---
+++ Ball.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+
+	public Transform ballTransform;
+	private Rigidbody _rb;
+	public GameObject player;
+	public bool hasBall = false;
+	public float shotPower = 100f;
+	public bool right;
+	public bool left;
+
+	void Start ()
+
+	{
+
+		_rb = GetComponent<Rigidbody> ();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+
+	{
+		
+
+
+		Shot ();
+
+		if (hasBall && right) 
+		{
+			transform.position = new Vector3 (player.transform.position.x + 1.053f, player.transform.position.y - 0.697f, player.transform.position.z );
+
+		}
+
+		if (hasBall && left)
+		{
+
+			transform.position = new Vector3 (player.transform.position.x - 1.053f, player.transform.position.y - 0.697f, player.transform.position.z );
+
+		}
+
+
+
+
+
+	}
+
+
+	private void OnCollisionEnter (Collision collision)
+	{
+
+
+		if (collision.gameObject.name == "Player") {
+			//Debug.Log ("Collision: " + collision.gameObject.name + ".");
+			Debug.Log ("Player has ball.");
+
+			hasBall = true;
+		}
+	}
+
+
+	public void Shot()
+	{
+
+
+		if(Input.GetButtonDown("Fire1") && hasBall && right)
+		{
+			hasBall = false;
+			_rb.AddForce (shotPower, shotPower / 3, 0);
+		}
+
+
+		if(Input.GetButtonDown("Fire1") && hasBall && left)
+		{
+			hasBall = false;
+
+			_rb.AddForce (-shotPower, shotPower/3, 0);
+		}
+
+
+
+
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+
+
+	[SerializeField]
+	private float jumpForce = 100f;
+	[SerializeField]
+	private float doubleJumpForce;
+	[SerializeField]
+	private bool canjump = true;
+	[SerializeField]
+	private bool canDoubleJump = false;
+	public bool moveRight = true;
+	public bool moveLeft = false;
+
+
+
+
+	private Rigidbody rb;
+
+	
+
+	public float moveSpeed = 5f;
+	private float moveHorizontal;
+	private float moveVertical;
+	private float jump;
+
+
+	Ball scriptBall;
+	GameObject ballObject;
+
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		ballObject = GameObject.Find ("Ball");
+		scriptBall = ballObject.GetComponent<Ball>();
+
+	}
+
+
+	void Update ()
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		MovePlayer();
+		TurnPlayer ();
+
+
+	}
+
+	// LateUpdate is called after all Update functions have been called
+	void LateUpdate()
+	{
+
+	}
+
+
+
+	void MovePlayer()
+	{
+		moveHorizontal = Input.GetAxis("Horizontal");
+		moveVertical = Input.GetAxis("Vertical");
+		jump = Input.GetAxis("Jump");
+
+		if (moveRight)
+		{
+			transform.Translate (Vector3.right * moveHorizontal * moveSpeed * Time.deltaTime);
+			transform.Translate (Vector3.forward * moveVertical * moveSpeed * Time.deltaTime);
+		}
+
+
+		if (moveLeft) 
+		{
+			transform.Translate (-Vector3.right * moveHorizontal * moveSpeed * Time.deltaTime);
+			transform.Translate (Vector3.back * moveVertical * moveSpeed * Time.deltaTime);
+
+		}
+
+
+
+
+
+		if (canDoubleJump && Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.velocity = new Vector3(0, jumpForce, 0);
+			canDoubleJump = false;
+
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && canjump)
+		{
+			rb.velocity = new Vector3(0, jumpForce, 0);
+
+
+			canjump = false;
+			canDoubleJump = true;
+		}
+
+
+
+
+
+
+
+
+	}
+	private void OnCollisionEnter (Collision collision)
+	{
+		
+
+		if (collision.gameObject.name == "Teren") {
+			//Debug.Log ("Collision: " + collision.gameObject.name + ".");
+			Debug.Log ("Player on ground.");
+
+			canjump = true;
+			canDoubleJump = false;
+		}
+	}
+
+	private void TurnPlayer()
+	{
+
+
+		if (Input.GetKey (KeyCode.A) && moveRight) 
+		{
+			Quaternion turnRotation = Quaternion.Euler (0f, 180f , 0f );
+			rb.MoveRotation (rb.rotation * turnRotation);
+			//moveLeft = true;
+			//scriptBall.left = true;
+			//moveRight = false;
+			//scriptBall.right = false;
+
+			SetLeftRight (true, false);
+
+		}
+		if (Input.GetKey (KeyCode.D) && moveLeft)
+		{
+		
+			Quaternion turnRotation = Quaternion.Euler (0f, 180f , 0f );
+			rb.MoveRotation (rb.rotation * turnRotation);
+
+			SetLeftRight (false, true);
+
+		}
+			
+	}
+
+	void SetLeftRight (bool l, bool r)
+	{
+		moveLeft = l;
+		scriptBall.left = l;
+		moveRight = r;
+		scriptBall.right = r;
+	}
+
+
+
+}
